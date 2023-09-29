939af06c63db90d661386c8a2bd0f850f05ec071
lboutouil <lboutouil@icloud.com>
2023-09-29 16:07:53 +0200

    première version

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pygame
 from rplidar import RPLidar
 import numpy as np
 
-PORT_NAME = 'COM3'
+
+PORT_NAME = 'COM3' #changer le port com selon le plug
 lidar = RPLidar(PORT_NAME)
+
 
 pygame.init()
 width, height = 800, 800
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("LiDAR Data")
+
 
 
 def display_lidar_data():
@@ -19,9 +22,8 @@ def display_lidar_data():
                 run = False
                 break
 
-        win.fill((0, 0, 0))
-
         for scan in lidar.iter_scans():
+            win.fill((0, 0, 0))
             for (_, angle, distance) in scan:
                 x = int(distance * np.cos(np.radians(angle)) / 10)
                 y = int(distance * np.sin(np.radians(angle)) / 10)
@@ -32,5 +34,7 @@ def display_lidar_data():
     lidar.stop()
     lidar.disconnect()
     pygame.quit()
+
+
 
 display_lidar_data()
