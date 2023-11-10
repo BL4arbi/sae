914e7a6072707b752146a4d981e58ea73a9671cf
lboutouil <lboutouil@icloud.com>
2023-11-10 08:36:14 +0100

    n ouveau essais a voir

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
-import pygame
-from rplidar import RPLidar
 import numpy as np
+from rplidar import RPLidar
+import matplotlib.pyplot as plt
 
-
-PORT_NAME = 'COM3' #changer le port com selon le pluge
+# Initialisation du LIDAR
+PORT_NAME = 'COM3'
 lidar = RPLidar(PORT_NAME)
 
+# Paramètres pour la grille de mapping
+MAX_RANGE_MM = 8000  # Portée maximale du LIDAR en millimètres (8 mètres)
+GRID_RESOLUTION = 0.05  # Résolution de la grille (5 cm par cellule de grille)
+GRID_SIZE = int((MAX_RANGE_MM / 1000) / GRID_RESOLUTION)  # Taille de la grille en cellules
 
-pygame.init()
-width, height = 800, 800
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("LiDAR Data")
+# Configuration de l'affichage avec matplotlib
+fig, ax = plt.subplots(figsize=(10, 10))
+lidar_position = (GRID_SIZE // 2, GRID_SIZE // 2)  # Position du LIDAR au centre de la grille
 
+# Fonction pour mettre à jour la grille avec les données du scan LIDAR
+def update_grid(angles, distances, lidar_position):
+    grid = np.full((GRID_SIZE, GRID_SIZE), 0.5)  # Initialisation de la grille avec des valeurs neutres
+    for angle, distance in zip(angles, distances):
+        if 0 < distance <= MAX_RANGE_MM:  # Filtrage des mesures valides
+            # Conversion des coordonnées polaires (angle, distance) en coordonnées cartésiennes (x, y)
+            # Les fonctions cos et sin sont utilisées pour cette conversion.
+            # cos(angle) donne la projection sur l'axe des x, tandis que sin(angle) donne celle sur l'axe des y.
+            x_mm = distance * np.cos(np.radians(angle))  # Conversion de l'angle en radians pour l'utiliser avec cos
+            y_mm = distance * np.sin(np.radians(angle))  # Conversion de l'angle en radians pour l'utiliser avec sin
 
+            # Conversion des coordonnées en millimètres en coordonnées de la grille
+            x_cell = int(x_mm / (GRID_RESOLUTION * 1000)) + lidar_position[0]
+            y_cell = int(y_mm / (GRID_RESOLUTION * 1000)) + lidar_position[1]
 
-def display_lidar_data():
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
+            # Vérification si les coordonnées sont dans les limites de la grille
+            if (0 <= x_cell < GRID_SIZE) and (0 <= y_cell < GRID_SIZE):
+                grid[x_cell, y_cell] = 1  # Marquage de la cellule comme occupée
+    return grid
 
-        for scan in lidar.iter_scans():
-            win.fill((0, 0, 0))
-            for (_, angle, distance) in scan:
-                x = int(distance * np.cos(np.radians(angle)) / 10)
-                y = int(distance * np.sin(np.radians(angle)) / 10)
-                pygame.draw.circle(win, (255, 255, 255), (width // 2 + x, height // 2 - y), 2)
-
-            pygame.display.update()
-
+# Exécution du LIDAR et mise à jour de la grille avec les scans
+try:
+    print("Démarrage du LIDAR...")
+    for scan in lidar.iter_scans():
+        angles = np.array([item[1] for item in scan])  # Extraction des angles du scan
+        distances = np.array([item[2] for item in scan])  # Extraction des distances du scan
+        grid = update_grid(angles, distances, lidar_position)  # Mise à jour de la grille avec les nouvelles données
+        ax.imshow(grid, cmap='hot', interpolation='nearest')  # Affichage de la grille
+        ax.plot(lidar_position[1], lidar_position[0], 'bo')  # Marquage de la position du LIDAR
+        plt.pause(0.1)  # Pause pour la mise à jour de l'affichage
+        ax.clear()  # Effacement de l'affichage pour le prochain scan
+finally:
     lidar.stop()
-    lidar.disconnect()
-    pygame.quit()
-
-
-
-display_lidar_data()
+    lidar.disconnect()  # Arrêt du LIDAR et fermeture de la connexion
--- simu_robot/simu.py
+++ simu_robot/simu.py
@@ -1,112 +1,41 @@
 import pygame
 import random
-from queue import PriorityQueue
+from collections import deque
 
-# Initialisation de pygame.
 pygame.init()
 
-# Dimensions de l'écran.
 WIDTH, HEIGHT = 800, 600
-
-# Taille des nœuds de la grille.
 NODE_SIZE = 20
 
-# Création de l'écran et d'une horloge pour gérer le framerate.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Simulation Robot Aspirateur avec Pathfinding")
 clock = pygame.time.Clock()
 
-
 class Node:
-    """Représente un nœud sur la grille."""
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.neighbors = []
-        self.g = float('inf')
-        self.f = float('inf')
-        self.previous = None
+        self.visited = False
 
     def get_pos(self):
-        """Retourne la position du nœud."""
         return self.x, self.y
-
-    def add_neighbors(self, grid):
-        """Ajoute les nœuds voisins à la liste des voisins."""
-        if self.x > 0:
-            self.neighbors.append(grid[self.x-1][self.y])
-        if self.x < len(grid) - 1:
-            self.neighbors.append(grid[self.x+1][self.y])
-        if self.y > 0:
-            self.neighbors.append(grid[self.x][self.y-1])
-        if self.y < len(grid[0]) - 1:
-            self.neighbors.append(grid[self.x][self.y+1])
-
-    def __lt__(self, other):
-        """Rend cette classe sortable en fonction de la valeur de f."""
-        return self.f < other.f
-
-def heuristic(node1, node2):
-    """Calcule une heuristique (distance Manhattan) entre deux nœuds."""
-    x1, y1 = node1.get_pos()
-    x2, y2 = node2.get_pos()
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
-def a_star(start, end):
-    """Implémentation de l'algorithme A* pour trouver le chemin le plus court entre deux nœuds."""
-    open_set = PriorityQueue()
-    open_set.put((0, start))
-    came_from = {}
-    start.g = 0
-    start.f = heuristic(start, end)
-    open_set_hash = {start}
-
-    while not open_set.empty():
-        current = open_set.get()[1]
-        open_set_hash.remove(current)
-
-        if current == end:
-            path = []
-            while current in came_from:
-                path.append(current)
-                current = came_from[current]
-            return path
-
-        for neighbor in current.neighbors:
-            temp_g = current.g + 1
-            if temp_g < neighbor.g:
-                came_from[neighbor] = current
-                neighbor.g = temp_g
-                neighbor.f = temp_g + heuristic(neighbor, end)
-                if neighbor not in open_set_hash:
-                    open_set.put((neighbor.f, neighbor))
-                    open_set_hash.add(neighbor)
-
-    return []
 
 cols = WIDTH // NODE_SIZE
 rows = HEIGHT // NODE_SIZE
 
 def create_grid():
-    """grille avec des nœuds et leurs voisinsee"""
     grid = [[Node(i, j) for j in range(rows)] for i in range(cols)]
-    for i in range(cols):
-        for j in range(rows):
-            grid[i][j].add_neighbors(grid)
     return grid
 
-# Initialisation de la grille, des positions de départ et d'arrivée, et des obstacles.
 grid = create_grid()
-start = (0, 0)
-end = (cols - 1, rows - 1)
+start = grid[0][0]
 
-walls = [random.choice([(random.randint(0, cols-1), random.randint(0, rows-1)) for _ in range(100)])]
+walls = [(random.randint(0, cols-1), random.randint(0, rows-1)) for _ in range(100)]
 
-path = a_star(grid[start[0]][start[1]], grid[end[0]][end[1]])
-current_pos = start
+visited_nodes = []
+queue = deque([start])
+future_path = list(queue)  # Initialize the future path with the start node
 
-# main en gros
 running = True
 while running:
     screen.fill((255, 255, 255))
@@ -115,41 +44,50 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    if random.randint(0, 10) == 0:  # Augmentation de la fréquence de déplacement des obstacles pas au point
+    if random.randint(0, 10) == 0:
         wall_to_move = random.choice(walls)
         walls.remove(wall_to_move)
         x = random.randint(0, cols-1)
         y = random.randint(0, rows-1)
         walls.append((x, y))
 
-        grid = create_grid()  # Recréer la grille en fonction des nouveaux obstacles marche pas mal
-        for wall in walls:
-            x, y = wall
-            grid[x][y].neighbors = []
+    if queue:
+        current_node = queue.popleft()
+        x, y = current_node.get_pos()
+        if (x, y) not in walls:
+            current_node.visited = True
+            visited_nodes.append(current_node)
 
-        path = a_star(grid[current_pos[0]][current_pos[1]], grid[end[0]][end[1]])
+            # Add unvisited neighbors to the queue
+            if x > 0 and grid[x-1][y] not in queue and grid[x-1][y] not in visited_nodes and (x-1, y) not in walls:
+                queue.append(grid[x-1][y])
+            if x < cols - 1 and grid[x+1][y] not in queue and grid[x+1][y] not in visited_nodes and (x+1, y) not in walls:
+                queue.append(grid[x+1][y])
+            if y > 0 and grid[x][y-1] not in queue and grid[x][y-1] not in visited_nodes and (x, y-1) not in walls:
+                queue.append(grid[x][y-1])
+            if y < cols - 1 and grid[x][y+1] not in queue and grid[x][y+1] not in visited_nodes and (x, y+1) not in walls:
+                queue.append(grid[x][y+1])
 
-    # draw tout
+    future_path = list(queue)  # Update the future path with the nodes in the queue
+
     for wall in walls:
         x, y = wall
-        pygame.draw.rect(screen, (0, 0, 0), (x*NODE_SIZE, y*NODE_SIZE, NODE_SIZE, NODE_SIZE))
+        pygame.draw.rect(screen, (0, 0, 0), (x * NODE_SIZE, y * NODE_SIZE, NODE_SIZE, NODE_SIZE))
 
-    for node in path:
+    for node in visited_nodes:
         x, y = node.get_pos()
-        pygame.draw.rect(screen, (0, 255, 0), (x*NODE_SIZE, y*NODE_SIZE, NODE_SIZE, NODE_SIZE))
+        pygame.draw.rect(screen, (0, 0, 255), (x * NODE_SIZE, y * NODE_SIZE, NODE_SIZE, NODE_SIZE))  # Blue for visited nodes
 
-    pygame.draw.rect(screen, (255, 0, 0), (current_pos[0]*NODE_SIZE, current_pos[1]*NODE_SIZE, NODE_SIZE, NODE_SIZE))
-    pygame.draw.rect(screen, (0, 0, 255), (end[0]*NODE_SIZE, end[1]*NODE_SIZE, NODE_SIZE, NODE_SIZE))
+    for node in future_path:
+        x, y = node.get_pos()
+        pygame.draw.rect(screen, (0, 255, 0), (x * NODE_SIZE, y * NODE_SIZE, NODE_SIZE, NODE_SIZE))  # Green for future path
 
-    # regarde les node pour se déplacer
-    if path and len(path) > 1:
-        next_node = path[-2]
-        next_pos = next_node.get_pos()
-        if next_pos not in walls:
-            current_pos = next_pos
-            path = a_star(grid[current_pos[0]][current_pos[1]], grid[end[0]][end[1]])
+    if visited_nodes:
+        # Red for the aspirateur
+        x, y = visited_nodes[-1].get_pos()
+        pygame.draw.rect(screen, (255, 0, 0), (x * NODE_SIZE, y * NODE_SIZE, NODE_SIZE, NODE_SIZE))
 
     pygame.display.flip()
-    clock.tick(5)
+    clock.tick(10)  # Adjust this value to slow down or speed up the animation
 
 pygame.quit()
